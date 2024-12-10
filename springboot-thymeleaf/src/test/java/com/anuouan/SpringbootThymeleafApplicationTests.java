3d759e00a7034b7dcf117ede9a673460a33c6e5c
anuouan <youcong@163.com>
2024-12-10 13:09:15 +0800

    thymeleaf

---
+++ springboot-thymeleaf/src/test/java/com/anuouan/SpringbootThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuouan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
