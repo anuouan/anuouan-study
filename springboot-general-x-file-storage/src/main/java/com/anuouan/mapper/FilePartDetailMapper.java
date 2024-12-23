460f3606886afa4fa1452387e3cc96a34208e95c
anuouan <youcong@163.com>
2024-12-23 18:21:33 +0800

    "使用OSS"

---
+++ springboot-general-x-file-storage/src/main/java/com/anuouan/mapper/FilePartDetailMapper.java
@@ -0,0 +1,6 @@
+package com.anuouan.mapper;
+
+import com.anuouan.model.FilePartDetail;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface FilePartDetailMapper extends BaseMapper<FilePartDetail> {}
